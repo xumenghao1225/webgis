55ff36163ac588db5f6f06a176b26165a0944660
xumenghao <xumenghao1225@163.com>
2024-10-15 20:04:39 +0800

    删除无效文件、添加mapbox-gl的依赖

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    nightwatchPlugin({
-      renderPage: './nightwatch/index.html'
-    }),
   ],
   resolve: {
     alias: {
